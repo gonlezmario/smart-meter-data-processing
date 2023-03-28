60f53ad8e7f4b2ea46eadb6f768a9744b96bf712
Mario Olego <88584700+gonlezmario@users.noreply.github.com>
2023-03-28 18:02:43 +0200

    Added pending measurements setter and getter

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,18 @@
-from sqlalchemy import Column, Integer, JSON, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+import configparser
+from typing import TypeVar
+
+from sqlalchemy import Column, Integer, JSON, String, Boolean, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+measurements = TypeVar('measurements', bound='Measurements')
+
 
 Base = declarative_base()
 
-class ElectricalData(Base):
-    __tablename__ = 'electrical_data'
+
+class Measurements(Base):
+    __tablename__ = 'measurements'
 
     id = Column(Integer, primary_key=True)
     timestamp = Column(Integer)
@@ -13,3 +21,29 @@ class ElectricalData(Base):
     power = Column(JSON)
     error = Column(String)
     processed = Column(Boolean, default=False)
+
+    @staticmethod
+    def _session_initialization() -> sessionmaker:
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+
+        engine = create_engine(config.get('database', 'url'))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        return session
+
+    @classmethod
+    def get_pending_measurements(cls) -> measurements:
+        session = cls._session_initialization()
+        measurements = session.query(cls).filter_by(pending=True, error='').all()
+        session.close()
+        return measurements
+
+    @classmethod
+    def set_not_pending(cls, measurement_id: int) -> None:
+        session = cls._session_initialization()
+        measurement = session.query(cls).filter_by(id=measurement_id).one_or_none()
+        if measurement:
+            measurement.pending = False
+            session.commit()
+        session.close()
